48a5cd9b7caa3d83e2295e1cd8b87124106e8f59
swathy06 <38415961+swathy06@users.noreply.github.com>
2018-07-11 15:06:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
-import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello World!"
-
+def wsgi_app(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    response_body = 'Hello World'
+    yield response_body.encode()
 
 if __name__ == '__main__':
-    app.run()
+    from wsgiref.simple_server import make_server
+
+    httpd = make_server('localhost', 5555, wsgi_app)
+    httpd.serve_forever()
